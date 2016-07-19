839f194f56a4fdb2afd05ba1fb987e7c8a490ea7
NazDov <nazardovhuy@gmail.com>
2016-07-20 00:51:28 +0300

    configuring project

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,12 +1,22 @@
 import {Component} from 'angular2/core';
+import {HomeNavBarComponent} from "./home_navbar.component";
+import {ROUTER_DIRECTIVES} from "angular2/router";
+import {RouteConfig} from "angular2/router";
+import {WhatsNewComponent} from "./whats_new.component";
 
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
+        <home-navbar></home-navbar>
+        <router-outlet></router-outlet>
     `,
+    directives: [HomeNavBarComponent, ROUTER_DIRECTIVES]
 })
+@RouteConfig(
+    [
+        {path: '/whats_new', name:'WhatsNew', component: WhatsNewComponent}
+    ]
+)
 export class AppComponent {
 
 }
---
+++ dev/home_navbar.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core'
+
+@Component(
+    {
+        selector: 'home-navbar',
+        templateUrl:'templates/home_navbar.html',
+    }
+)
+export class HomeNavBarComponent{
+
+}
